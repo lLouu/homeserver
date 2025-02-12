8b4662a98bba6bd8908fb40fc172864b90db5f9b
lLouu <lLouu@users.noreply.github.com>
2025-02-12 12:09:51 +0100

    Hotfix 2.1

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ echo "[+] Hostname ip setted to $ip"
 echo "[~] Adding proxmox VE repo"
 echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" | sudo tee /etc/apt/sources.list.d/pve-install-repo.list > /dev/null
 wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O ~/proxmox-release-bookworm.gpg >/dev/null
-if [[ "$(sha512sum ~/proxmox-release-bookworm.gpg)" != "7da6fe34168adc6e479327ba517796d4702fa2f8b4f0a9833f5ea6e6b48f6507a6da403a274fe201595edc86a84463d50383d07f64bdde2e3658108db7d6dc87 ~/proxmox-release-bookworm.gpg" ]]; then
+if [[ "$(sha512sum ~/proxmox-release-bookworm.gpg | awk '{print($1)}')" != "7da6fe34168adc6e479327ba517796d4702fa2f8b4f0a9833f5ea6e6b48f6507a6da403a274fe201595edc86a84463d50383d07f64bdde2e3658108db7d6dc87" ]]; then
     echo "[-] Failed to fetch gpg key for proxmox repo"
     rm ~/proxmox-release-bookworm.gpg >/dev/null 2>/dev/null
     exit 1
